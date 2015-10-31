3dccf55d5ace604337fe16409ae48030fc39524b
cem <jimcc333@gmail.com>
2015-10-31 12:05:26 -0500

    adds parser

--- src/classes.cpp
+++ src/classes.cpp
@@ -2,6 +2,15 @@
 
 #include "classes.h"
 
+void RegionInfo::Print() {
+    cout << "Region thickness: " << thickness << ". Isos and number densities:" << endl;
+
+    for(map<string, float>::const_iterator it = NumDens.begin(); it != NumDens.end(); ++it) {
+        cout << it->first << " " << it->second << endl;
+    }
+}
+
+
 // Prints what it's holding to terminal
 void ParamsHolder::Print() {
     cout << " Database folder: " << data_path << endl
@@ -15,10 +24,31 @@ void ParamsHolder::ReadIso() {
     if(!input.is_open()) {cout << "Couldn't open input file!" << endl; return;}
 
     string line;
+    string name;
+    float value;
+
     while(getline(input, line)) {
-        if(!line.compare("NUMBER DENSITIES")) {cout << "YAY" << endl;}
+        if(!line.compare("Input begin")) {
+                cout << "Reading region 1" << endl;
 
-        cout << line[0] << endl;
+                while(getline(input, line), line[0] != 'X') {
+                    // Read each line until the thickness (X)
+
+                    istringstream iss(line);
+                    iss >> name >> value;
+
+                    region.NumDens[name] = value;
+
+                }
+
+                // Store the thickness value
+                istringstream iss(line);
+                iss >> name >> value;
+
+                region.thickness = value;
+        }
+        ///TODO add next region parsing method
+
     }
-
+    region.Print();
 }
--- src/classes.h
+++ src/classes.h
@@ -1,21 +1,31 @@
 #include <iostream>
+#include<map>
 
 #include <eigen3/Eigen/Core>
 #include <vector>
 
 using namespace std;
 
+class RegionInfo {
+public:
+    float thickness;
+    map<string,float> NumDens;
+
+    void Print();
+};
+
 class ParamsHolder {
 public:
+    // Global values
     unsigned int egroups = 10;
     string data_path = "./Data/";
     string input_path = "./Input/input.txt";
+
+    // Problem objects
+    RegionInfo region;
 
     void Print();
     void ReadIso();
 };
 
 
-class Isotope {
-    Isotope();
-};
