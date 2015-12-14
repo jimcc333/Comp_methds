15f6bf3279541f4c8b49784af6d9ff09daff5c66
jimcc333 <jimcc333@gmail.com>
2015-12-13 23:19:09 -0600

    adds silly ascii art

--- src/main.cpp
+++ src/main.cpp
@@ -130,6 +130,18 @@ void OutputGen(Phi &phi, ParamsHolder &params) {
 }
 
 int main(int argc, char* argv[]) {
+    cout << "  _Cem's Super-Awesome Transport Code_" << endl << endl;
+    cout << "    .---------------------------." << endl;
+    cout << "   /,--..---..---..---..---..--. `." << endl;
+    cout << "  //___||___||___||___||___||___\\_|" << endl;
+    cout << "  [j__ ######################## [_|" << endl;
+    cout << "     \\============================|" << endl;
+    cout << "  .==|  |'''||'''||'''||'''| |'''||" << endl;
+    cout << " /======'---''---''---''---'=|  =||" << endl;
+    cout << " |____    TRANSPORT    ____  | ==||" << endl;
+    cout << " //  \\\\         BUS   //  \\\\ |===||" << endl;
+    cout << " \\\\__//---------------\\\\__//-+---+'" << endl << endl;
+
     // Defaults
     ParamsHolder params;
 
